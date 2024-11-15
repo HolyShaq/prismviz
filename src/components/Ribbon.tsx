489dbd1fa74ab2d62b31462bf8a76a02ad28329b
HolyShaq <acebyronehalili@gmail.com>
2024-11-15 18:30:26 +0800

    feat: dont show ribbon button at all when disabled

--- src/components/Ribbon.tsx
+++ src/components/Ribbon.tsx
@@ -23,16 +23,16 @@ const RibbonButton: React.FC<RibbonButtonProps> = ({
   onClick,
   enabled,
 }) => {
-  return (
+  return enabled ? (
     <div
       onClick={onClick}
       className="flex flex-col w-16 space-y-1 items-center"
     >
-      <div
-        className={`h-10 aspect-square ${enabled ? "bg-blue-500" : "bg-slate-500"}`}
-      />
+      <div className="h-10 aspect-square bg-slate-500" />
       <span className="text-xs text-wrap text-center w-max-full">{label}</span>
     </div>
+  ) : (
+    <></>
   );
 };
 
