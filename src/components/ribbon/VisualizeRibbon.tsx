2883fe555853405584b3a8637c7ee362533383c2
HolyShaq <acebyronehalili@gmail.com>
2024-11-18 15:11:44 +0800

    feat: added popover for add chart button

--- src/components/ribbon/VisualizeRibbon.tsx
+++ src/components/ribbon/VisualizeRibbon.tsx
@@ -1,18 +1,84 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useRef } from "react";
 import RibbonButton from "./RibbonButton";
 import { CsvContext } from "../../lib/CsvContext";
 import { useStepContext } from "../../lib/StepContext";
 
 // Material UI
 import BarChartIcon from "@mui/icons-material/BarChart";
+import PieChartIcon from "@mui/icons-material/PieChart";
+import ShowChartIcon from "@mui/icons-material/ShowChart";
+import StackedLineChartIcon from "@mui/icons-material/StackedLineChart";
+import ScatterPlotIcon from "@mui/icons-material/ScatterPlot";
 import TextFieldsIcon from "@mui/icons-material/TextFields";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import Popover from "@mui/material/Popover";
 
-const VisualizeRibbon: React.FC<{ left?: boolean; right?: boolean }> = ({
+// Interfaces
+interface VisualizeRibbonProps {
+  left?: boolean;
+  right?: boolean;
+}
+interface PopoverButtonProps {
+  Icon: React.ElementType;
+  label: string;
+}
+
+// Static Components
+const PopoverButton: React.FC<PopoverButtonProps> = ({ Icon, label }) => {
+  return (
+    <div
+      className="w-24 h-24 rounded-md flex flex-col justify-center items-center outline
+        text-[#313154] hover:text-white
+        bg-[#b4b4b4]
+        hover:bg-[#545469]
+        outline-[#545469]"
+    >
+      <Icon className="text-6xl" />
+      <p className="text-sm">{label}</p>
+    </div>
+  );
+};
+
+const VisualizeRibbon: React.FC<VisualizeRibbonProps> = ({
   left = false,
   right = false,
 }) => {
   // Initialization
+  const addChartRef = useRef(null);
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
+  // Components
+  const AddChartPopover: React.FC = () => {
+    return (
+      <Popover
+        open={isPopoverOpen}
+        onClose={() => setIsPopoverOpen(false)}
+        anchorEl={addChartRef.current}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <div className="flex flex-col space-y-6 p-6 bg-white rounded-xl shadow-md items-center">
+          <div className="flex flex-row space-x-6">
+            <PopoverButton Icon={BarChartIcon} label="Bar Chart" />
+            <PopoverButton Icon={PieChartIcon} label="Pie Chart" />
+          </div>
+          <div className="flex flex-row space-x-6">
+            <PopoverButton Icon={ShowChartIcon} label="Line Chart" />
+            <PopoverButton Icon={StackedLineChartIcon} label="Area Chart" />
+          </div>
+          <div className="flex flex-row space-x-6">
+            <PopoverButton Icon={ScatterPlotIcon} label="Scatter Chart" />
+          </div>
+        </div>
+      </Popover>
+    );
+  };
 
   // Functions
 
@@ -26,9 +92,13 @@ const VisualizeRibbon: React.FC<{ left?: boolean; right?: boolean }> = ({
     left: [
       // Left Ribbon Buttons
       <RibbonButton
+        ref={addChartRef}
         key={0}
         Icon={BarChartIcon}
-        onClick={() => {}}
+        onClick={() => {
+          setIsPopoverOpen(true);
+          console.log("Adding a chart...");
+        }}
         enabled={true}
         tooltip="Add a Chart: Visualize your data with a chart"
       />,
@@ -64,6 +134,7 @@ const VisualizeRibbon: React.FC<{ left?: boolean; right?: boolean }> = ({
     <>
       {left ? VisualizeButtonSet.left.map((button) => button) : null}
       {right ? VisualizeButtonSet.right.map((button) => button) : null}
+      <AddChartPopover />
     </>
   );
 };
